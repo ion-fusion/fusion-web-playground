879cd2c553e54565befdbaf8bbb8f8dee052f265
Isaac M. Good <imgoodproductions@sbcglobal.net>
2025-04-29 14:34:53 -0700

    Add logging for runtime initialization errors
    The deployed version on GitHub pages is producing an error I can't
    reproduce running on either my personal hosted version or a debug
    build on my laptop. This should help debug that.

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,12 @@ mod fusion_sandbox;
 use fusion_sandbox::FusionSandbox;
 
 fn main() {
+    // Release builds should only log at info+, dev builds log at debug+
+    #[cfg(not(debug_assertions))]
+    let config = wasm_logger::Config::new(log::Level::Info);
+    #[cfg(debug_assertions)]
+    let config = wasm_logger::Config::new(log::Level::Debug);
+    wasm_logger::init(config);
+
     yew::Renderer::<FusionSandbox>::new().render();
 }
